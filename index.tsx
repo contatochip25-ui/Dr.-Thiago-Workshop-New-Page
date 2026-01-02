b848775cb96d7c959d5c1950e86a20cb4b77d2f0
contatochip25-ui <contatochip25@gmail.com>
2026-01-02 05:23:17 -0300

    novo

--- index.tsx
+++ index.tsx
@@ -3,15 +3,18 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+console.log("Iniciando renderização da aplicação...");
+
 const container = document.getElementById('root');
 
 if (!container) {
-  throw new Error("Não foi possível encontrar o elemento root para montar a aplicação");
+  console.error("Erro fatal: Elemento #root não encontrado no DOM.");
+} else {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  console.log("React montado com sucesso.");
 }
-
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
